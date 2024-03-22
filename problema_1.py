950b3095706d1b04521c44e464ce5ba43aaa6d3a
Carlos <95112759+Carlos-JPS@users.noreply.github.com>
2024-03-22 09:42:16 -0300

    Add files via upload

---
+++ problema_1.py
@@ -0,0 +1,17 @@
+#Problema 1  / 8 ptos x4 pruebas / 32 puntos
+#Concatenación de listas o tuplas
+#--------------------------------
+#Confeccione un programa que lea 2 tuplas sean t1 y t2
+#La salida debe ser una tupla en el orden t2 t1 t2
+#---------------------------------
+#Ejemplo de entrada:
+#         20 90 hola
+#		  mundo 44
+#La salida debe ser
+#         ('mundo', 44, 20, 90, 'hola', 'mundo', 44)
+t = input()
+m = input()
+
+t1 = tuple(t.split())
+t2 = tuple(m.split())
+print(t1 + t2 + t1)
